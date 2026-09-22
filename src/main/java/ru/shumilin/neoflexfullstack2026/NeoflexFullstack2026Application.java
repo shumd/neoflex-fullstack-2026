c9b71085ba75e1bb3534e5eedbaf32818accd410
danil <danilsuperworld@gmail.com>
2026-09-22 19:32:17 +0400

    init

---
+++ src/main/java/ru/shumilin/neoflexfullstack2026/NeoflexFullstack2026Application.java
@@ -0,0 +1,13 @@
+package ru.shumilin.neoflexfullstack2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoflexFullstack2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoflexFullstack2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/shumilin/neoflexfullstack2026/NeoflexFullstack2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shumilin.neoflexfullstack2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflexFullstack2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
